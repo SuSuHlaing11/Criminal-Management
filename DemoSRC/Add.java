89ac35a7586ad64df3ed10992d4b758559598413
SuSuHlaing11 <115403289+SuSuHlaing11@users.noreply.github.com>
2024-02-15 21:48:38 +0630

    fxml files of Admin page, Add page
    Demo

---
+++ DemoSRC/Add.java
@@ -0,0 +1,28 @@
+// Must have the application package
+// Add vmarg to launch.json for java runtime {name: admin} {type: java}
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Add extends Application {
+
+  public static void main(String[] args) throws Exception {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    FXMLLoader fxmlLoader = new FXMLLoader(
+      getClass().getClassLoader().getResource("Add.fxml")
+    );
+    Parent root = fxmlLoader.load();
+    Scene scene = new Scene(root);
+
+    primaryStage.setTitle("Admin Page");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+}
