d62627b56afec884b5bec4f52cfc4d8c9c177f18
SuSuHlaing11 <115403289+SuSuHlaing11@users.noreply.github.com>
2024-02-23 22:00:37 +0630

    no commit! ^-^ --|--

---
+++ src/scene1Controller.java
@@ -0,0 +1,15 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class scene1Controller {
+
+  @FXML
+  private ResourceBundle resources;
+
+  @FXML
+  private URL location;
+
+  @FXML
+  void initialize() {}
+}
